98882e015249eb965001823b52b6f304eb8fe672
Michelle Lam <shelly2.lam@gmail.com>
2019-05-24 19:52:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -75,10 +75,10 @@ function placeOrder(cardNumber) {
   if(cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   }
-  else {
-  var total = total()
-    // cart = []
+  //else {
+  var total = total();
+    cart = [];
     return `Your total cost is ${total}, which will be charged to the card ${cardNumber}.`
-  }
+ // }
 }
 
